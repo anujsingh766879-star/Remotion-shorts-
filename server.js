c2d91f7e5b2a29b2275cb51fedfe0f6ad8710815
anujsingh766879-star <anujsingh766879@gmail.com>
2026-09-22 13:30:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ const OUTPUT_DIR = path.join(__dirname, "out");
 if (!fs.existsSync(OUTPUT_DIR)) fs.mkdirSync(OUTPUT_DIR);
 
 let bundleLocationPromise = null;
+function getBundle() {
+  if (!bundleLocationPromise) {
+    bundleLocationPromise = bundle({
+      entryPoint: path.join(__dirname, "index.ts"),
+    });
+  }
+  return bundleLocationPromise;
+}
 entryPoint: path.join(__dirname, "index.ts"),
 
 // Simple shared-secret auth so random people on the internet can't burn your Railway hours
